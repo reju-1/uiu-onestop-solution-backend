170a0d548535739f32758a0a1995e88fe0ac9aa4
reju-1 <rahmed1445@gmail.com>
2024-04-16 23:26:36 +0600

    locations & subjects

---
+++ .data/subject.js
@@ -0,0 +1,17 @@
+export default {
+  // add all the CSE subject hare
+  CSE: [
+    ["subject code", "Subject Name"],
+    ["demo code 1", "Structure programme language"],
+    ["demo code 2", "Object Oriented Programming"],
+    [],
+  ],
+
+  // add 30 the EEE subject hare
+  EEE: [
+    ["subject code", "Subject Name"],
+    ["subject code", "Subject Name"],
+    ["subject code", "Subject Name"],
+    [],
+  ],
+};
