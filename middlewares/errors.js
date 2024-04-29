c36ca8e936a04bf9d0d61cd441b448478cef209c
reju-1 <rahmed1445@gmail.com>
2024-04-29 20:03:10 +0600

    better error handling

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,9 +1,22 @@
+import multer from "multer";
+
 function notFoundHandler(req, res, next) {
   next("Requested resource is not found");
 }
+
 function commonErrorHandler(err, req, res, next) {
-  //console.log(err);
-  res.json({ Error: err });
+  if (err instanceof multer.MulterError) {
+    if (err.code === "LIMIT_FILE_SIZE") {
+      // 413 Payload Too Large
+      return res.status(413).json({ error: "File size too large" });
+    } else {
+      // Handle other Multer errors
+      return res.status(400).json({ error: "File upload error" });
+    }
+  } else {
+    // For other errors
+    return res.status(500).json({ error: err });
+  }
 }
 
 export { commonErrorHandler, notFoundHandler };
